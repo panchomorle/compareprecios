376d0f4f56963d3c45c14f5585e8c4e56cc5eaf3
Juan Pablo Morales <eljuampimorales@gmail.com>
2024-02-04 22:51:33 -0300

    objetizacion de cards

--- src/Market.jsx
+++ src/Market.jsx
@@ -1,15 +1,36 @@
 import { Card } from "./Card";
-import "./Market.css"
+import "./Market.css";
+
+const cards = [];
+
+cards.push({
+    "Titulo": "Yerba mate",
+    "Descripcion": "precios variables entre 200 y 300",
+});
+
+cards.push({
+    "Titulo": "Manaos",
+    "Descripcion": "precios variables entre 400 y 500",
+});
+
+cards.push({
+    "Titulo": "vino toro",
+    "Descripcion": "precios variables entre 400 y 500",
+});
+
+cards.push({
+    "Titulo": "carne de res",
+    "Descripcion": "precios variables entre 400 y 500",
+});
 
 export function Market (){
+    {console.log(cards)}
     return(
     <div className="marketBox">
-    <Card titulo="Kg de pan" descripcion="Precios variables entre 500 y 1000"/>
-    {/*
-    <Card titulo={"volkswagen gol mod 97"} descripcion={"Precios variables entre 400 y 800"}/>
-    <Card titulo={"Arroz parboil dos hermanos"} descripcion={"Precios variables entre 1200 y 3000"}/>
-    <Card titulo={"alfajor terrabusi clasico"} descripcion={"Precios variables entre 1200 y 3000"}/>
-    */}
+            {cards.map((card, index) => (
+                    <Card titulo={card.Titulo} descripcion={card.Descripcion} key={index}></Card>
+            ))
+            }
     </div>
     )
 }
